df0b660e0ff227115ac4ec80d6e8e20c24fad9c7
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-04-08 00:04:35 -0400

    css

--- src/nameComponent.js
+++ src/nameComponent.js
@@ -4,8 +4,10 @@ setCandidate,
 error})=> {
     return (
         <section>
+            <h2
+            style={{margin: '0 0 0 2rem'}}>Enter your name to take a general knowledge test</h2>
         <input
-        style={{fontSize: '2rem', margin: '2rem  0 0 15rem',
+        style={{fontSize: '2rem', margin: '2rem  0 0 10rem',
     borderWidth: '3px'}}
         type='text'
         value={candidate}
