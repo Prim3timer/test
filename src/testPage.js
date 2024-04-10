11dad3d106e938e20930c1dd9bc8e0261fbef742
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-04-10 10:33:21 -0400

    still on portrait

--- src/testPage.js
+++ src/testPage.js
@@ -50,8 +50,6 @@ useEffect(()=>{
                       >
 
                       <h4 id="clock"
-                      style={{backgroundColor: 'teal',
-                    color: 'white'}}
                       >{clock < 60 ? `:${clock % 60}` : clock % 60 >= 10 ? `${Math.floor(clock / 60)} : ${clock % 60}` : clock < 10 ? 0`${clock % 60}`:   `${Math.floor(clock / 60)} : 0${clock % 60}`
                       }</h4><span id="time-up"
                       style={{fontWeight:'bold'}}
