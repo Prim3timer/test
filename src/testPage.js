c251cc4d5362828e0e64faaad4aec2033f01c45d
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-07-14 16:05:17 -0400

    color of clock, size of numbers

--- src/testPage.js
+++ src/testPage.js
@@ -68,7 +68,7 @@ useEffect(()=>{
                       >
                       <h5 id="clock"
                       style={{margin: '0 1rem',
-                        color: 'tomato'
+                        color: 'purple'
                       }}
                   >{clock < 10 ? `:0${clock % 60}` : clock < 60 ? `:${clock % 60}`  : clock % 60 >= 10 ? `${Math.floor(clock / 60)}:${clock % 60}` : clock < 10 ? 0`${clock % 60}`:   `${Math.floor(clock / 60)}:0${clock % 60}`
                   }</h5>
