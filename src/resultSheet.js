716ff5571ebe66fda3b34ea16c838be7ec3b15a1
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-05-10 11:53:51 -0400

    submit at time up

--- src/resultSheet.js
+++ src/resultSheet.js
@@ -33,8 +33,8 @@ console.log(score)
         <article id='result-table'>
             <main id='cred' 
             style={{
-                marginTop: '2rem',
-                marginBottom: '1rem',
+                marginTop: '4rem',
+                // marginBottom: '30rem',
             //    display: 'flex',
             //    alignItems: 'flexStart',
             flexDirection: 'row',
@@ -42,7 +42,7 @@ console.log(score)
             // flexBasis: '5%',
             //    columnGap: '1rem',
             // rowGap: '3rem',
-               marginLeft: '1rem'
+               marginLeft: '1rem',
                 // backgroundColor: 'green'
             }}
             >
